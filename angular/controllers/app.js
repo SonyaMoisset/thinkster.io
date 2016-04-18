0cbc3b257320a7503a81766d7f0327117e2ced4b
SonyaMoisset <sonya.moisset@gmail.com>
2016-04-18 17:15:15 +0900

    Add controllers example folder

---
+++ angular/controllers/app.js
@@ -0,0 +1,11 @@
+angular.module('app', []);
+
+angular.module('app').controller('MainCtrl', function ($scope){
+  var self = this;
+
+  self.message = 'hello';
+
+  self.updateMessage = function(message){
+    self.message = message;
+  };
+});
